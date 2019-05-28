6813b86bc9e037a8364801c368477ec5194bc76c
Inyama, Uchechukwu <depoulet@yahoo.com>
2019-05-28 14:41:31 +0100

    updated the webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,17 @@ module.exports = {
 	entry: './src/main.js',
 	output: {
 		filename: './bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!sass-loader'
+			},
+			{
+
+			}
+		]
+		
 	}
 };
